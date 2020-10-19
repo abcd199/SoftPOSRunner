057d57b989079a3431a4f0956ba2f3230bb80ec8
Narayan Bandwalkar <narayan8692b@gmail.com>
2020-10-19 13:18:18 +0530

    final
    Signed-off-by: Narayan Bandwalkar <narayan8692b@gmail.com>

--- app/src/main/java/com/cpoc/softpostest/Utils.java
+++ app/src/main/java/com/cpoc/softpostest/Utils.java
@@ -1,23 +1,34 @@
 package com.cpoc.softpostest;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+
+import java.util.List;
 
 public class Utils {
 
     public static final String PREF_SESSIONID = "sessionId";
 
-    public static boolean isPackageInstalled(String packageName, PackageManager packageManager) {
-        try {
-            return packageManager.getApplicationInfo(packageName, 0).enabled;
-        } catch (PackageManager.NameNotFoundException e) {
-            return false;
-        }
-    }
+    //    public static boolean isPackageInstalled(String packageName, PackageManager packageManager) {
+//        try {
+//            return packageManager.getApplicationInfo(packageName, 0).enabled;
+//        } catch (PackageManager.NameNotFoundException e) {
+//            return false;
+//        }
+//    }
+    public static boolean isPackageInstalled(String targetPackage, PackageManager pm) {
+        List<ApplicationInfo> packages;
 
+        packages = pm.getInstalledApplications(0);
+        for (ApplicationInfo packageInfo : packages) {
+            if (packageInfo.packageName.equals(targetPackage))
+                return true;
+        }
+        return false;
+    }
 
     public static void setToken(Context context, String token) {
         SharedPreferences.Editor editor = getSharedPreferences(context).edit();
